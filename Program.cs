95933e22618335f93fb6c461702f25830b189f93
Santiagitk10 <93751091+Santiagitk10@users.noreply.github.com>
2023-03-27 09:51:37 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
 //dotnet build //para construir el programa y ver que esté todo correcto
 //dotnet restore //para restaurar todas las dependencias que tiene el proyecto
 //dotnet watch run //cuando se hace un cambio en el proyecto vuelve a compilarse el proyecto y ejecutarse
+//dotnet clean //elimina todo el contenido de la carpeta bin que se generó durante la última compilación para volver a compilar
+//dotnet buil --configuration release //para hacer un buil mucho más optimo para funcionar en producción
 
 using Humanizer;
 
